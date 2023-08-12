5ee7fe1d8bc70bebb6ca3abf9a6a0b9d844d58e5
adas0mahdi <adas0mahdi@gmail.com>
2023-08-12 12:03:04 +0800

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,41 +30,7 @@ const navMenu = document.getElementById('nav-menu'),
           el: '.swiper-scrollbar',
         },
       });
-      
-      swiper.on('slideNextTransitionStart', function() {
-        // Trigger the GSAP animation
-       
-          TweenMax.from('.home__title', 1, {delay: .2, opacity: 0, y: 20, ease: Expo.easeInOut})
-          TweenMax.from('.home__description', 1, {delay: .3, opacity: 0, y: 20, ease: Expo.easeInOut})
-          TweenMax.from('.home__button', 1, {delay: .4, opacity: 0, y: 20, ease: Expo.easeInOut})
-          TweenMax.from('.home__liquid', 1, {delay: .7, opacity: 0, y: 200, ease: Expo.easeInOut})
-          TweenMax.from('.home__juice-animate', 1, {delay: 1.2, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__apple1', 1, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__apple2', 1, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__leaf:nth-child(1)', 2, {delay: 1.3, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__leaf:nth-child(2)', 2, {delay: 1.4, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__leaf:nth-child(3)', 2, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__leaf:nth-child(4)', 2, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__leaf:nth-child(5)', 2, {delay: 1.7, opacity: 0, y: -800, ease: Expo.easeInOut})
-          TweenMax.from('.home__leaf:nth-child(6)', 2, {delay: 1.8, opacity: 0, y: -800, ease: Expo.easeInOut})
-      });
-      
-      swiper.on('slidePrevTransitionStart', function() {
-        // Trigger the GSAP animation
-        TweenMax.from('.home__title', 1, {delay: .2, opacity: 0, y: 20, ease: Expo.easeInOut})
-        TweenMax.from('.home__description', 1, {delay: .3, opacity: 0, y: 20, ease: Expo.easeInOut})
-        TweenMax.from('.home__button', 1, {delay: .4, opacity: 0, y: 20, ease: Expo.easeInOut})
-        TweenMax.from('.home__liquid', 1, {delay: .7, opacity: 0, y: 200, ease: Expo.easeInOut})
-        TweenMax.from('.home__juice-animate', 1, {delay: 1.2, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__apple1', 1, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__apple2', 1, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__leaf:nth-child(1)', 2, {delay: 1.3, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__leaf:nth-child(2)', 2, {delay: 1.4, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__leaf:nth-child(3)', 2, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__leaf:nth-child(4)', 2, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__leaf:nth-child(5)', 2, {delay: 1.7, opacity: 0, y: -800, ease: Expo.easeInOut})
-        TweenMax.from('.home__leaf:nth-child(6)', 2, {delay: 1.8, opacity: 0, y: -800, ease: Expo.easeInOut})
-      });
+     
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if(navToggle){
@@ -158,26 +124,26 @@ TweenMax.from('.home__leaf:nth-child(6)', 2, {delay: 1.8, opacity: 0, y: -800, e
 
 
 
-// window.addEventListener('scroll', function() {
-//   // Check if the user has scrolled back to the top of the page
-//   if (window.scrollY === 0) {
-//     // Trigger the GSAP animation
-//     TweenMax.from('.home__title', 1, {delay: .2, opacity: 0, y: 20, ease: Expo.easeInOut});
-//     TweenMax.from('.home__description', 1, {delay: .3, opacity: 0, y: 20, ease: Expo.easeInOut})
-//     TweenMax.from('.home__button', 1, {delay: .4, opacity: 0, y: 20, ease: Expo.easeInOut})
-//     TweenMax.from('.home__liquid', 1, {delay: .7, opacity: 0, y: 200, ease: Expo.easeInOut})
-//     TweenMax.from('.home__juice-animate', 1, {delay: 1.2, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__apple1', 1, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__apple2', 1, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__leaf:nth-child(1)', 2, {delay: 1.3, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__leaf:nth-child(2)', 2, {delay: 1.4, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__leaf:nth-child(3)', 2, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__leaf:nth-child(4)', 2, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__leaf:nth-child(5)', 2, {delay: 1.7, opacity: 0, y: -800, ease: Expo.easeInOut})
-//     TweenMax.from('.home__leaf:nth-child(6)', 2, {delay: 1.8, opacity: 0, y: -800, ease: Expo.easeInOut})
+window.addEventListener('scroll', function() {
+  // Check if the user has scrolled back to the top of the page
+  if (window.scrollY === 0) {
+    // Trigger the GSAP animation
+    TweenMax.from('.home__title', 1, {delay: .2, opacity: 0, y: 20, ease: Expo.easeInOut});
+    TweenMax.from('.home__description', 1, {delay: .3, opacity: 0, y: 20, ease: Expo.easeInOut})
+    TweenMax.from('.home__button', 1, {delay: .4, opacity: 0, y: 20, ease: Expo.easeInOut})
+    TweenMax.from('.home__liquid', 1, {delay: .7, opacity: 0, y: 200, ease: Expo.easeInOut})
+    TweenMax.from('.home__juice-animate', 1, {delay: 1.2, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__apple1', 1, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__apple2', 1, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__leaf:nth-child(1)', 2, {delay: 1.3, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__leaf:nth-child(2)', 2, {delay: 1.4, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__leaf:nth-child(3)', 2, {delay: 1.5, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__leaf:nth-child(4)', 2, {delay: 1.6, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__leaf:nth-child(5)', 2, {delay: 1.7, opacity: 0, y: -800, ease: Expo.easeInOut})
+    TweenMax.from('.home__leaf:nth-child(6)', 2, {delay: 1.8, opacity: 0, y: -800, ease: Expo.easeInOut})
 
-//   }
-// });
+  }
+});
 
 
 // window details popup 
